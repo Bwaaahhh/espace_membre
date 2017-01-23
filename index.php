a74f5b4ecb312f746089a068836ec00360b8e5c6
Bwaaahhh <cy.etievant@gmail.com>
2017-01-23 15:49:41 +0100

    Add logo+start index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Velkroala</title>
+        <meta name="description" content="Travail Espace Membres" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/foundation/6.2.4-rc2/foundation.min.css"  />
+        <link rel="stylesheet" type="text/css" href="view/css/style.css"  />
+    </head>
+    <body>
+        <header>
+            <div class="row">
+                <img class="small-12 large-2 column" src="view/images/logo.png" alt="une tete de koala qui est le logo du site" />
+                <div class="small-12 large-3 large-offset-2 column">
+                    <h1>Velkroala :</h1>
+                    <p>Vous ne pourrez jamais en décrocher</p>
+                </div>
+                <div id="login" class="small-12 large-3 large-offset-2 column">
+                    <input type="text" placeholder="Identifiant"/>
+                    <input type="password" placeholder="Mot de passe"  />
+                    <button type="submit">Zou!</button>
+                </div>
+            </div>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
