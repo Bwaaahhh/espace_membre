71ab542e3985b1f502ce1eab9e2075408deca75a
Bwaaahhh <cy.etievant@gmail.com>
2017-01-23 16:11:02 +0100

    Add form in index

--- index.php
+++ index.php
@@ -10,21 +10,30 @@
     <body>
         <header>
             <div class="row">
-                <img class="small-12 large-2 column" src="view/images/logo.png" alt="une tete de koala qui est le logo du site" />
+                <img class="small-3 small-offset-4 large-2 large-offset-0 column" src="view/images/logo.png" alt="une tete de koala qui est le logo du site" />
                 <div class="small-12 large-3 large-offset-2 column">
                     <h1>Velkroala :</h1>
                     <p>Vous ne pourrez jamais en décrocher</p>
                 </div>
-                <div id="login" class="small-12 large-3 large-offset-2 column">
+                <div id="login" class="small-5 small-offset-3 large-3 large-offset-2 column end">
                     <input type="text" placeholder="Identifiant"/>
                     <input type="password" placeholder="Mot de passe"  />
-                    <button type="submit">Zou!</button>
+                    <button class="button success" type="submit">Zou!</button>
                 </div>
             </div>
         </header>
 
         <main>
-
+            <p>S'inscrire :</p>
+            <form>
+                <input type="text" name="" value="">
+                <input type="text" name="" value="">
+                <input type="text" name="" value="">
+                <input type="text" name="" value="">
+                <input type="password" name="" value="">
+                <input type="password" name="" value="">
+                <button class="button primary" type="submit">S'inscrire</button>
+            </form>
         </main>
 
         <footer>
