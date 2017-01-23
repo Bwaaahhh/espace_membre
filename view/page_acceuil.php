a67c0b5a758a107594ccf366de7ed7a1491443e5
Unknown <manu_guillou@yahoo.fr>
2017-01-23 15:49:37 +0100

    page accueil mise en page foundation

---
+++ view/page_acceuil.php
@@ -0,0 +1,46 @@
+<body>
+	<header>
+		<div class="row">
+			<div class="small-8 large-2 columns">
+				<img src="..."/>
+			</div>
+			<div class="small-8 large-2 columns">
+				<h1>Velkroala</h1>
+			</div>
+			<div class="small-8 large-2 columns">
+				<p>Bonjour ...</p>
+			</div>
+		</div>
+	</header>
+	<section>
+		<div class="row">
+			<div class="small-8 large-4 columns">
+				<div>
+					<p>Bienvenue petit Koala !</p>
+				</div>
+			</div>
+			<div class="small-8 large-4 columns">
+				<img alt="Koala content" src="..."/>
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-10 large-5 columns">
+				<img alt="Gros bouton de modification de profil"/>
+			</div>
+			<div class="small-8 large-4 columns">
+				<p>Rechercher un membre</p>
+				<form>
+					<input type="text"></input>
+					<input type="submit"></input>
+				</form>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="small-8 large-4 columns">
+			<img src="..." alt="logo-facebook"/>
+			<img src="..." alt="logo-twitter"/>
+			<img src="..." alt="logo-github"/>
+		</div>
+	</footer>
+</body>
