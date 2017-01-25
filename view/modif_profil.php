4c75b6d9d702f12027450185df78ce19df41cad6
S.Melanie <melanie.s@codeur.online>
2017-01-25 11:17:52 +0100

    modification profil fonctionne

--- view/modif_profil.php
+++ view/modif_profil.php
@@ -18,17 +18,17 @@
                 <h2>Si tu veux modifier ton profil tu es au bon endroit petit Koala</h2>
             </div>
         </div>
-        <form class="" action="index.html" method="post">
+        <form class="" action="../controller/php/requete_modif.php" method="post">
             <div class="row">
                 <div class="small-12 large-3 large-offset-1 columns">
                     <img src="" alt="">
                     <button type="button" name="button">Modifier l'image de profil</button>
                 </div>
                 <div class="small-12 large-5 large-offset-2 columns end">
-                    <input type="text" name="" value="" placeholder="Votre pseudo" />
-                    <input type="text" name="" value="" placeholder="Votre Nom">
-                    <input type="text" name="" value="" placeholder="Votre Prenom">
-                    <input type="text" name="" value="" placeholder="Votre email">
+                    <input type="text" name="pseudo" value="" placeholder="Votre pseudo" />
+                    <input type="text" name="nom" value="" placeholder="Votre Nom">
+                    <input type="text" name="prenom" value="" placeholder="Votre Prenom">
+                    <input type="text" name="mail" value="" placeholder="Votre email">
                 </div>
             </div>
             <div class="row">
@@ -38,7 +38,7 @@
             </div>
             <div class="row">
                 <div class="small-4 large-2 large-centered columns">
-                    <button class="success button" type="submit">Modifier</button>
+                    <button class="success button" name="modifier" type="submit">Modifier</button>
                 </div>
             </div>
         </form>
