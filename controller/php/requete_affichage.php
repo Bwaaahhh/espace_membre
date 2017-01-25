c429e101a208a1ec9e2c0d1d68e1b997d04805a5
Bwaaahhh <cy.etievant@gmail.com>
2017-01-25 13:39:59 +0100

    Modif requete_affichage add url_image

--- controller/php/requete_affichage.php
+++ controller/php/requete_affichage.php
@@ -1,6 +1,6 @@
 <?php
 	// request to display one profil
 	$profil = $_GET["pseudo"];
-	$query = $dbh->query("SELECT pseudo , nom , prenom , description FROM utilisateur WHERE utilisateur.ID_utilisateur = '$profil'");
+	$query = $dbh->query("SELECT pseudo , nom , prenom , description , url_image FROM utilisateur WHERE utilisateur.ID_utilisateur = '$profil'");
 	$res = $query->fetchAll(PDO::FETCH_OBJ);
 ?>
