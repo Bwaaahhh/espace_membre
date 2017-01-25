0a0485eb722f5960d9704f9d124f38b580ce98c0
Unknown <quangnhanbui@gmail.com>
2017-01-25 15:39:07 +0100

    update login

--- controller/php/login.php
+++ controller/php/login.php
@@ -1,7 +1,9 @@
 <?php
 
 if(isset($_POST['login_submit'])){
+
   include "functions.php";
+
 	$errors = array();
 
 
@@ -10,6 +12,7 @@ if(isset($_POST['login_submit'])){
 		$errors[] = "entrer un pseudo";
     echo "entrer un pseudo";
 	}
+
   else{
 		$username = $_POST['identification'];
     echo $username;
@@ -17,17 +20,17 @@ if(isset($_POST['login_submit'])){
 
 	if(empty($_POST['passwordid'])){
 		$errors[] = "entrer votre mdp";
-    echo "entrer un mdp";
+    echo "";
 	}
+
   else{
 		$password = $_POST['passwordid'];
     echo $password;
 	}
 
-	if (empty($errors)) {
+	if (count($errors == 0)) {
 		$sql = "SELECT ID_utilisateur FROM utilisateur WHERE pseudo = '$username'";
-    debug($sql);
-		$result = $dbh->query($sql);
+		$result = $mysqli->query($sql);
 		if ($result->num_rows === 1) {
 			$row = $result->fetch_array(MYSQLI_ASSOC);
 			if (password_verify($password, $row['passwordid'])) {
