f4b4451cdf1c8a8f32168f1a7bd97f7828b4aa39
Unknown <manu_guillou@yahoo.fr>
2017-01-25 11:24:39 +0100

    visu_profil searching pseudo ok

--- view/visu_profil.php
+++ view/visu_profil.php
@@ -19,11 +19,8 @@
             <?php foreach ($res as $obj):?>
 
             <div class="small-12 large-2 columns">
-                <?php if($obj->url_image == ""){ ?>
-                    <?php echo "Cet utilisateur n'a pas encore d'image de profil" ?>
-                <?php }else{ ?>
-                    <img src="<?=$obj->url_image?>" alt="Image représentant l'avatar du profil">
-                <?php } ?>
+
+                <img src="<?=$obj->url_image?>" alt="Image représentant l'avatar du profil">
             </div>
             <div class="small-12 large-4 large-offset-1 columns">
                 <label name="pseudo_profil">Pseudo : <?=$obj->pseudo?></label>
@@ -36,10 +33,10 @@
 					<input type="text" name="un_membre" value="">
 					<input type="submit">
 				</form>
-				<?php if(isset($_POST["un_membre"])){ ?>
+			<?php if(isset($_POST["un_membre"])){ ?>
 				<form name="member_find" action='visu_profil.php' method="post">
 					<?php foreach($result as $each){ ?>
-						<a href="index.php?page=visu_profil"><?=$member?></a>
+						<a href="index.php?page=visu_profil&pseudo=<?=$each['ID_utilisateur']?>"><?=$each['pseudo']?></a>
 					<?php } ?>
 				</form>
 				<?php } ?>
