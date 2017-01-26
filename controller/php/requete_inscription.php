af60c1320e2b6034027c82bc8c4df08ab13addb2
Unknown <quangnhanbui@gmail.com>
2017-01-26 10:35:42 +0100

    nickname and email can't be the same

--- controller/php/requete_inscription.php
+++ controller/php/requete_inscription.php
@@ -18,10 +18,28 @@ include 'PDO.php';
   if (empty($_POST['nickname']) || !preg_match('/^[a-z\d_]{2,20}$/i', $_POST['nickname'] )) {
     $errors['nickname'] = "le pseudo n'est pas valide";
   }
+  else{
+    $req = $dbh->prepare("SELECT ID_utilisateur FROM utilisateur WHERE pseudo = ?")
+    $req->execute([$_POST['nickname']]);
+    $user = $req->fetch();
+    if($user){
+      $errors['nickname'] = 'Ce pseudo est déjà pris';
+      echo "<p>Ce pseudo est déjà pris<p>";
+    }
+  }
 
   if (empty($_POST['email']) || !filter_var($_POST['email'] )) {
 
     $errors['email'] = "L'adresse électronique n'est pas valide ";
+  }
+  else{
+    $req = $dbh->prepare("SELECT ID_utilisateur FROM utilisateur WHERE mail = ?")
+    $req->execute([$_POST['email']]);
+    $user = $req->fetch();
+    if($user){
+      $errors['email'] = 'Cet e-mail est déjà pris';
+      echo "<p>Cet e-mail est déjà pris<p>";
+    }
   }
 
   if (empty($_POST['password']) || $_POST['password'] != $_POST['reconfirm'] ) {
